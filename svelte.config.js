faa3329563569c8e20171e966dd56d2791c3db50
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-12-31 19:00:44 +0000

    svelte.config modified to ssg, see desc.
    - credits to @CaptainCodeman

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,27 @@
-import adapter from '@sveltejs/adapter-static';
+// Credits to CaptainCodeman : @https://github.com/CaptainCodeman/svelte-theme-select/blob/master/svelte.config.js
+
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
+
+const prod = process.env.NODE_ENV === 'production' /* NODE_ENV=production npm run build */
+const repo_name = "sveltekit-ssg-boilerplate"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	// preprocess: [vitePreprocess()], /* <= not required at the moment */
+
 	kit: {
 		adapter: adapter({
-			fallback: 'app.html'
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
 		}),
-		prerender: { entries: [] },
-		outDir: "./docs"
+		paths: {
+			base: prod ? repo_name : '',
+		},
 	},
-};
+}
 
-export default config;
+export default config
