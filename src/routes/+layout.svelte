886b3a6aac50ffc30702d4c05bb7c8114da54f39
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-01-21 22:16:11 +0000

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,7 @@
 
     <slot />
 
-<h3>
-    <!-- footer -->
-    <div class="btn-group">
-    <button class="btn">1</button>
-    <button class="btn btn-active">2</button>
-    <button class="btn">3</button>
-    <button class="btn">4</button>
-  </div>
-</h3>
+<h3>Footer</h3>
 
 <style>
     @tailwind base;
